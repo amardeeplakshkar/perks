ceb00e844c50f91936231d81acc7620397f88375
amardeeplakshkar <109571797+amardeeplakshkar@users.noreply.github.com>
2024-10-21 15:13:04 +0530

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const getRandomBrightColorClass = () => {
   const brightColors = [
-    "bg-yellow-500", 
-    "bg-green-400", 
-    "bg-pink-500", 
-    "bg-blue-500", 
-    "bg-red-500", 
-    "bg-purple-500", 
-    "bg-teal-400", 
-    "bg-orange-500"
+    "bg-yellow-500",
+    "bg-green-400",
+    "bg-pink-500",
+    "bg-blue-500",
+    "bg-red-500",
+    "bg-purple-500",
+    "bg-teal-400",
+    "bg-orange-500",
   ];
   return brightColors[Math.floor(Math.random() * brightColors.length)];
 };
 
-const Card = ({ username = "Cocks User", amount, rank, bg }) => {
-  const randomColorClass = getRandomBrightColorClass(); 
+const Card = ({ username = "Cocks User", amount, rank, bg, telegramId }) => {
+  const randomColorClass = getRandomBrightColorClass();
+  const profileImageUrl = `https://api.ratingtma.com/cdn/user/${telegramId}`;
 
   return (
     <div
-      className={` flex justify-between items-center p-2 rounded-lg w-full m-1 ${bg}`}
+      className={`flex justify-between items-center p-2 rounded-lg w-full m-1 ${bg}`}
     >
       <div className="flex justify-center items-center gap-2">
-        <span
-          className={`h-[3rem] text-black font-bold aspect-square flex justify-center items-center rounded-full uppercase ${randomColorClass}`}
-        >
-          {username.slice(0, 2)} 
-        </span>
+        <div
+          className={`h-[3rem] w-[3rem] aspect-square rounded-full bg-cover bg-center ${randomColorClass}`}
+          style={{ backgroundImage: `url(${profileImageUrl})` }}
+        />
         <div>
           <h4 className="text-sm font-bold">{username}</h4>
           {amount !== undefined && (
